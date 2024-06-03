4e5364a21b7c39b844ffcf5df67485da583f16d7
a-sh-dev <iam.angie@ymail.com>
2024-06-04 00:08:01 +1000

    Update next.config for permanet redirection for /characters page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/info/characters',
+        destination: '/info/characters/1',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
