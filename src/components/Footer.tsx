bac3241eef14ab2b8d1cce340d75c5a06c51c404
a-sh-dev <iam.angie@ymail.com>
2024-06-08 16:14:10 +1000

    Fix Footer text alignment to center

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Center, Link, Text } from '@chakra-ui/react'
 export const Footer = () => {
   return (
     <Center as="footer" h="3rem" mt={3}>
-      <Text fontSize="small">
+      <Text fontSize="small" textAlign="center">
         Crafted with ❤️ (within a very limited time) by{' '}
         <Link
           color="purple.500"
